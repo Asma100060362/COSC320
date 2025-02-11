6621503b601975d3f0e012180cab1bc3f25e5c1f
Asma100060362 <100060362@ku.ac.ae>
2025-02-11 10:58:42 +0400

    Add files via upload

---
+++ 12_options/main.rs
@@ -0,0 +1,37 @@
+fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
+    if hour_of_day > 23 {
+        None 
+    } else if hour_of_day >= 22 {
+        Some(0) 
+    } else {
+        Some(5) 
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn raw_value() {
+    
+        let icecreams = maybe_icecream(12).unwrap();
+
+        assert_eq!(icecreams, 5);
+    }
+
+    #[test]
+    fn check_icecream() {
+        assert_eq!(maybe_icecream(0), Some(5));
+        assert_eq!(maybe_icecream(9), Some(5));
+        assert_eq!(maybe_icecream(18), Some(5));
+        assert_eq!(maybe_icecream(22), Some(0));
+        assert_eq!(maybe_icecream(23), Some(0));
+        assert_eq!(maybe_icecream(24), None);
+        assert_eq!(maybe_icecream(25), None);
+    }
+}
