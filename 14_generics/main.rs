2f5fb8a4ad2f990ea7b567265daa0d157fbf8542
Asma100060362 <100060362@ku.ac.ae>
2025-02-18 09:53:01 +0400

    Add files via upload

---
+++ 14_generics/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+   
+    let mut numbers: Vec<i16> = Vec::new();
+
+    // Don't change the lines below.
+    let n1: u8 = 42;
+    numbers.push(n1.into());
+    let n2: i8 = -1;
+    numbers.push(n2.into());
+
+    println!("{numbers:?}");
+}
